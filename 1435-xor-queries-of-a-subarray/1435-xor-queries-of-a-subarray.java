335637ebc6d798bbdd4f1011789a15c767933cf2
AnmolKumar74125 <54698801+AnmolKumar74125@users.noreply.github.com>
2024-09-21 22:10:40 +0530

    Time: 2 ms (99.95%), Space: 60.4 MB (6.25%) - LeetHub

--- 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.java
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.java
@@ -3,12 +3,10 @@ class Solution {
         
         int n = arr.length;
         int left[] = new int[n+1];
-        int right[] = new int[n+1];
 
         for(int i = 0; i<n;i++)
         {
             left[i+1] = left[i] ^ arr[i];
-            right[n-i-1] = right[n-i] ^ arr[n-i-1];
         }
 
         int ans[]=new int[queries.length];

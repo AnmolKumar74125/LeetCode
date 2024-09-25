4ea88dad54dec8b89d28443d4d3bca071e440add
ANMOL KUMAR <54698801+AnmolKumar74125@users.noreply.github.com>
2024-09-26 01:42:27 +0530

    Time: 55 ms (5.03%), Space: 45.4 MB (9.01%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -0,0 +1,53 @@
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+
+        int len1 = s1.length();
+        int len2 = s2.length();
+        if(len1+len2 != s3.length())
+        {
+            return false;
+        }
+        String mat[][] = new String[len1+1][len2+2];
+        mat[0][0] = "";
+        for(int i = 1; i <= len1; i++)
+        {
+            Arrays.fill(mat[i],"");
+            mat[i][0] = s1.substring(0,i);
+        }    
+        for(int i = 1; i <= len2; i++)
+        {
+            mat[0][i] = s2.substring(0,i);
+        }
+
+        for(int i = 1; i <= len1; i++)
+        {
+            for(int j = 1; j <= len2; j++)
+            {
+                String part = s3.substring(0, (i+j));
+                //System.out.println( part +"  "+ (mat[i][0]+mat[0][j]) +"  "+(mat[0][j]+mat[j][0]));
+                if(part.equals(mat[i][0]+mat[0][j]) || part.equals(mat[0][j]+mat[i][0]) || 
+                    part.equals(mat[i-1][j]+s1.charAt(i-1)) || part.equals(mat[i][j-1]+s2.charAt(j-1)))
+                   // part.equals(mat[i-1][j-1]+s1.charAt(i-1)+s2.charAt(j-1)) ||
+                   // part.equals(mat[i-1][j-1]+s2.charAt(j-1)+s1.charAt(i-1)) )
+                {
+                    mat[i][j] = part;
+                }
+                else
+                {
+                    mat[i][j] = mat[i-1][j-1];
+                }
+            }
+        }
+/*
+        for(int i = 0; i <= len1; i++)
+        {
+            for(int j = 0; j <= len2; j++)
+            {
+                System.out.printf("%15s  ",mat[i][j]);
+            }
+            System.out.println();
+        }
+        */
+        return mat[len1][len2].equals(s3);
+    }
+}

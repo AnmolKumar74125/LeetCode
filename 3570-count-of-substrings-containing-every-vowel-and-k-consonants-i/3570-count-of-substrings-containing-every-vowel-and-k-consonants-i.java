f4f719b5ac88ff122337547b7eeabf6f3400cd22
ANMOL KUMAR <54698801+AnmolKumar74125@users.noreply.github.com>
2024-09-30 02:19:08 +0530

    Time: 3 ms (100%), Space: 42.7 MB (100%) - LeetHub

--- 3570-count-of-substrings-containing-every-vowel-and-k-consonants-i/3570-count-of-substrings-containing-every-vowel-and-k-consonants-i.java
+++ 3570-count-of-substrings-containing-every-vowel-and-k-consonants-i/3570-count-of-substrings-containing-every-vowel-and-k-consonants-i.java
@@ -1,9 +1,10 @@
 class Solution {
     public int countOfSubstrings(String word, int k) {
-        
+
         Map<Character, Integer> hm =new HashMap<Character, Integer>();        
         int count = 0,cons = 0;
         int i = 0, j = 0;
+        int nextCons = getNextCons(0,word);
         while(i < word.length())
         {
             char ch = word.charAt(i);
@@ -19,24 +20,13 @@ class Solution {
             {
                 if(cons == k)
                 {
-                    count ++;
-                }
-                int x = i;
-                while( x < word.length()-1 && cons == k)
-                {
-                    ch = word.charAt(x+1);
-                    if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                    if( nextCons <= i)
                     {
-                        count++;
+                        nextCons = getNextCons(i+1, word);
                     }
-                    else
-                    {
-                        break;
-                    }
-                    x++;
+                    count = count + (nextCons - i) ;
                 }
                 ch = word.charAt(j);
-                  System.out.println(hm);
                 if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
                 {
                     hm.put(ch, hm.get(ch)-1);
@@ -55,5 +45,17 @@ class Solution {
             i++;
         }
         return count;
+    }
+    public int getNextCons(int i, String word)
+    {
+        for( ;i< word.length();i++)
+        {
+            char ch = word.charAt(i);
+            if(!(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'))
+            {
+                return i;
+            }
+        }
+        return word.length();
     }
 }

0c88b1408ed114a620e0f6a183f2511ea8349ecc
ANMOL KUMAR <54698801+AnmolKumar74125@users.noreply.github.com>
2024-09-30 02:16:47 +0530

    Time: 162 ms (100%), Space: 47 MB (100%) - LeetHub

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.java
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.java
@@ -4,7 +4,6 @@ class Solution {
         Map<Character, Integer> hm =new HashMap<Character, Integer>();        
         long count = 0,cons = 0;
         int i = 0, j = 0;
-        int n = word.length();
         int nextCons = getNextCons(0,word);
         while(i < word.length())
         {
@@ -17,44 +16,17 @@ class Solution {
             {
                 cons ++;
             }
-             // System.out.print("i = "+i+" j = "+j+"cons = "+cons+"  A = ");
-             // System.out.println(hm);
             while(hm.size() == 5 && cons >= k)
             {
                 if(cons == k)
                 {
-                    //count ++;
-                    if( nextCons <= i && nextCons != -1)
+                    if( nextCons <= i)
                     {
-                            nextCons = getNextCons(i+1, word);
+                        nextCons = getNextCons(i+1, word);
                     }
-                    if( nextCons == -1)
-                    {
-                        count = count + (n-i);
-                    }
-                    else
-                    {
-                        count = count + (nextCons - i) ;
-                    }
+                    count = count + (nextCons - i) ;
                 }
-               /* int x = i;
-                while( x < word.length()-1 && cons == k)
-                {
-                    ch = word.charAt(x+1);
-                    if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
-                    {
-                    //    break;
-                        count++;
-                    }
-                    else
-                    {
-                        break;
-                    }
-                    x++;
-                }
-                */
                 ch = word.charAt(j);
-                  //System.out.println(hm);
                 if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
                 {
                     hm.put(ch, hm.get(ch)-1);
@@ -70,8 +42,6 @@ class Solution {
                 
                 j++;
             }
- //           if(hm.size() == 5 && cons == k)
- //               count ++;
             i++;
         }
         return count;
@@ -86,6 +56,6 @@ class Solution {
                 return i;
             }
         }
-        return -1;
+        return word.length();
     }
 }

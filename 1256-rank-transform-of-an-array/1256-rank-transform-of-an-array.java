ac71a38398266fa8b2cbb7cb194af819fe54759a
ANMOL KUMAR <54698801+AnmolKumar74125@users.noreply.github.com>
2024-10-02 13:02:42 +0530

    Time: 49 ms (16.86%), Space: 60.4 MB (80.08%) - LeetHub

---
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+
+        Map<Integer, Integer> hm = new HashMap<Integer,Integer>();
+        
+        int sortedArr[] = Arrays.stream(arr).distinct().sorted().toArray();
+        for(int i = 0; i < sortedArr.length; i++)
+        {
+            hm.put(sortedArr[i], i + 1);
+        }
+        int ans[] = new int[arr.length];
+        for(int i = 0; i <  arr.length; i++)
+        {
+            ans[i] = hm.get(arr[i]);
+        }
+        return ans;
+    }
+}

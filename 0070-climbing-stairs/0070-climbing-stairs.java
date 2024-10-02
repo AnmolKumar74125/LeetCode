3402d2d5fefc90288856b7eceed83fe910688a92
ANMOL KUMAR <54698801+AnmolKumar74125@users.noreply.github.com>
2024-10-03 01:20:38 +0530

    Time: 16 ms (100%), Space: 41.2 MB (6.37%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climbStairs(int n) {
+        
+        int a = 1,b = 2;
+        if(n <= 3)
+        {
+            return n;
+        }
+        int c = 0;
+        for(int i = 3; i <= n; i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+            System.out.println(a+" "+b+" "+c);
+        }
+        return c;
+    }
+}

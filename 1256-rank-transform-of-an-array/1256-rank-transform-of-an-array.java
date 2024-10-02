34225fb1164c8e6ef9f14d49f2f850266f635112
ANMOL KUMAR <54698801+AnmolKumar74125@users.noreply.github.com>
2024-10-02 13:05:30 +0530

    Time: 50 ms (15.79%), Space: 61.3 MB (75.29%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
@@ -7,7 +7,6 @@ class Solution {
         {
             hm.put(sortedArr[i], i + 1);
         }
-        //int ans[] = new int[arr.length];
         for(int i = 0; i <  arr.length; i++)
         {
             arr[i] = hm.get(arr[i]);

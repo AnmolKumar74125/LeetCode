5ebd39a7add4ef5cda3fe0c6e708ef29add3958c
ANMOL KUMAR <54698801+AnmolKumar74125@users.noreply.github.com>
2024-10-04 04:52:34 +0530

    Time: 65 ms (5.5%), Space: 69.1 MB (5.05%) - LeetHub

--- 1694-make-sum-divisible-by-p/1694-make-sum-divisible-by-p.java
+++ 1694-make-sum-divisible-by-p/1694-make-sum-divisible-by-p.java
@@ -1,25 +1,39 @@
 class Solution {
     public int minSubarray(int[] nums, int p) {
-    int sum = 0;    
-    for(int x : nums)
-    sum = (sum + x)%p;
-  
-    int mod = sum%p; 
-    if(mod==0)
-    return 0;
 
-    int ans = nums.length;
-    HashMap<Integer, Integer>hm = new HashMap<>();
-    hm.put(0, -1);
-    int currSumMod = 0;
-    for(int i=0; i<nums.length; i++)
-    {
-      currSumMod = (currSumMod+nums[i])%p;
-      int neededMod = (currSumMod - mod + p)%p;
-      if(hm.containsKey(neededMod))
-      ans = Math.min(ans, i-hm.get(neededMod));
-      hm.put(currSumMod, i);
-    }
-     return ans==nums.length ? -1 : ans;
+        int n = nums.length;
+        int val = 0;
+        for(int i = 0; i < n; i++)
+        {
+            nums[i] = nums[i] % p;
+            val = (val + nums[i]) % p;
+        }
+        if(val == 0)
+        {
+            return 0;
+        }
+        Map<Integer,Integer> hm =new HashMap<Integer,Integer>();
+        int curr = 0, ans = Integer.MAX_VALUE;
+        hm.put(0 , -1);
+        System.out.println("val = "+val);
+        System.out.println(hm);
+        for(int i = 0; i < n; i++)
+        {
+            curr = (curr + nums[i]) % p;
+            int key = ((curr - val) + p) % p;
+            //System.out.println(key);
+            if(hm.containsKey(key))
+            {
+                ans = Math.min(ans, (i - hm.get(key)));
+            }
+            hm.put(curr, i);
+            //System.out.println(hm);
+        }   
+        System.out.println(hm);
+        if(ans == Integer.MAX_VALUE || ans == n)
+        {
+            ans = -1;
+        }
+        return (int)ans;
     }
 }

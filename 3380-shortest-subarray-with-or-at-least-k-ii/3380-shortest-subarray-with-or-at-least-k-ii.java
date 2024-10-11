b7a200830514192f91fc04899ae58135a86f357c
ANMOL KUMAR <54698801+AnmolKumar74125@users.noreply.github.com>
2024-10-11 15:38:26 +0530

    Time: 45 ms (87.97%), Space: 69.7 MB (69.92%) - LeetHub

---
+++ 3380-shortest-subarray-with-or-at-least-k-ii/3380-shortest-subarray-with-or-at-least-k-ii.java
@@ -0,0 +1,70 @@
+class Solution {
+    public int minimumSubarrayLength(int[] nums, int k) {
+        
+        int min = Integer.MAX_VALUE;
+        int i = 0, j = 0, n = nums.length;
+        int val = 0;
+        int arr[] = new int[32];
+        while(i < n)
+        {
+            val = 0;
+            int p = nums[i];
+            for(int x = 0; x < 32; x++)
+            {
+                if(p > 0)
+                {
+                    if((p & 1) == 1)
+                    {
+                        arr[x]++;
+                    }
+                    p = p >> 1;
+                }
+                else
+                {
+                    break;
+                }
+                
+                //System.out.print(arr[x]+" ");
+            }
+            for(int x = 31; x >= 0; x--)
+            {
+                val = (val << 1) | ((arr[x] > 0) ? 1 : 0);
+            }
+           // System.out.println();
+           // System.out.println("A val = "+val);
+            while(j <= i && val >= k)
+            {
+                min = Math.min(min, i-(j-1));
+                p = nums[j];
+                val = 0;
+                for(int x = 0; x < 32; x++)
+                {
+                    if(p > 0)
+                    {
+                        if((p & 1) == 1)
+                        {
+                            arr[x]--;
+                        }
+                        p = p >> 1;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                for(int x = 31; x >= 0; x--)
+                {
+                    val = (val << 1) | ((arr[x] > 0) ? 1 : 0);
+                }
+                //System.out.println("B val = "+val);
+                j++;
+            }
+            i++;
+        }
+        if(min == Integer.MAX_VALUE)
+        {
+            return -1;
+        }
+        return min;
+    }
+}

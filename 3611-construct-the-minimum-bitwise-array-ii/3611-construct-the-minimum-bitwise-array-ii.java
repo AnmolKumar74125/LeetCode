8d19faaad2704cae3a571b6544bbbe7a9bf4edf8
ANMOL KUMAR <54698801+AnmolKumar74125@users.noreply.github.com>
2024-10-19 04:38:20 +0530

    Time: 56 ms (5.17%), Space: 45.4 MB (8.76%) - LeetHub

---
+++ 3611-construct-the-minimum-bitwise-array-ii/3611-construct-the-minimum-bitwise-array-ii.java
@@ -0,0 +1,76 @@
+class Solution {
+    public int[] minBitwiseArray(List<Integer> nums) {
+
+        int n = nums.size();
+        int ans[] = new int[n];
+
+        for(int i = 0; i < n; i++)
+        {
+            /*
+            int val = (int)Math.pow(2, count(nums.get(i))-1);
+            if((val | (val - 1)) == nums.get(i))
+            {
+                ans[i] = val - 1;
+            }
+            else if((val | (val + 1)) == nums.get(i))
+            {
+                ans[i] = val;
+            }
+            else
+            {
+                ans[i] = -1;
+            }
+            System.out.println(val);
+            */
+
+            int val = nums.get(i);
+            int c = count(nums.get(i));
+            int v = (int) Math.pow(2, c) - 1;
+            if( v == val)
+            {
+                int x = (int) Math.pow(2, c - 1);
+                if( (x | x-1) == val)
+                {
+                    ans[i] = x-1;
+                }
+                else
+                {
+                    ans[i] = -1;
+                }
+                continue;
+            }
+            System.out.println(Integer.toBinaryString(val));
+            if((val & 1 ) == 0)
+            {
+                ans[i] = -1;
+            }
+            else
+            {
+                //ans[i] = ((val >> 1) << 1) ;
+                char ch[] = Integer.toBinaryString(val).toCharArray();
+                for(int j = ch.length-1; j > 0; j--)
+                {
+                    if(ch[j] == '1' && ch[j-1] =='0')
+                    {
+                        ch[j] = '0';
+                        break;
+                    }
+                }
+                ans[i] = Integer.parseInt(new String(ch),2);
+            }
+            System.out.println("ans[i] = "+ Integer.toBinaryString(ans[i]));
+        }
+        return ans;
+    }
+
+    int count(int n)
+    {
+        int c = 0;
+        while(n > 0 && (n & 1) != 0)
+        {
+            c++;
+            n = n >> 1;
+        }
+        return c;
+    }
+}

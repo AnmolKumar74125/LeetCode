783918129168003bdb7c774cad97205e5bd72a3f
ANMOL KUMAR <54698801+AnmolKumar74125@users.noreply.github.com>
2024-10-24 03:04:26 +0530

    Time: 1716 ms (5.3%), Space: 76.5 MB (78.41%) - LeetHub

---
+++ 2677-cousins-in-binary-tree-ii/2677-cousins-in-binary-tree-ii.java
@@ -0,0 +1,86 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode replaceValueInTree(TreeNode root) {
+        
+        Queue<TreeNode> que = new LinkedList();
+        List<TreeNode> list = new ArrayList<>();
+        List<Integer> sumArr = new ArrayList<>();
+        que.add(root);
+        int sum = 0;
+        while(!que.isEmpty())
+        {
+            TreeNode front = que.poll();
+            if(front.left != null)
+            {
+                list.add(front.left);
+            }
+            if(front.right != null)
+            {
+                list.add(front.right);
+            }
+            sum = sum + front.val;
+            if(que.isEmpty())
+            {
+                que.addAll(list);
+                list = new ArrayList<>();
+                sumArr.add(sum);
+                sum = 0;
+            }
+        }
+        int i = 1;
+        System.out.println(sumArr);
+        que.add(root);
+        root.val = 0;
+        TreeNode ans = new TreeNode(0);
+       // Queue<TreeNode> que = new LinkedList();
+       // List<TreeNode> list = new ArrayList<>();
+        while(!que.isEmpty() && i < sumArr.size())
+        {
+            TreeNode front = que.poll();
+            int value = sumArr.get(i);
+            System.out.print(value+"  ");
+            if(front.left != null)
+            {
+                list.add(front.left);
+                value = value - front.left.val;
+                System.out.print(front.left.val+"  ");
+            }
+            if(front.right != null)
+            {
+                list.add(front.right);
+                value = value - front.right.val;
+                front.right.val = value;
+
+                System.out.print(front.right.val+"  ");
+            }
+            if(front.left != null)
+            {
+                front.left.val = value;
+
+                System.out.println(value+"  ");
+            }
+            
+            if(que.isEmpty())
+            {
+                que.addAll(list);
+                list = new ArrayList<>();     
+                i++;      
+            }
+        }
+        return root;
+    }
+}

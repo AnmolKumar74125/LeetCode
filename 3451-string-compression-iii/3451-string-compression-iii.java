0b94ff3dc17f5644dda5767dc2feabddb96927ba
ANMOL KUMAR <54698801+AnmolKumar74125@users.noreply.github.com>
2024-11-05 04:55:39 +0530

    Time: 39 ms (20.98%), Space: 46.4 MB (36.97%) - LeetHub

--- 3451-string-compression-iii/3451-string-compression-iii.java
+++ 3451-string-compression-iii/3451-string-compression-iii.java
@@ -1,18 +1,48 @@
 class Solution {
     public String compressedString(String word) {
-        StringBuilder comp = new StringBuilder();
-        int cnt = 1, n = word.length();
-        char ch = word.charAt(0);
-        for (int i = 1; i < n; i++) {
-            if (word.charAt(i) == ch && cnt < 9) {
-                cnt++;
-            } else {
-                comp.append(cnt).append(ch);
-                ch = word.charAt(i);
-                cnt = 1;
-            }
+        
+        StringBuilder ans = new StringBuilder();
+        //String ans = "";
+        int i = 1, count = 1;
+        int n = word.length();
+        char prev = word.charAt(0);
+        if( n == 1)
+        {
+            return "1" + word;
         }
-        comp.append(cnt).append(ch);
-        return comp.toString();
+        while(i < n)
+        {
+            char ch = word.charAt(i);
+            if(prev == ch)
+            {
+                if(count == 9)
+                {
+                    ans.append(count + "" + prev);
+                    //ans = ans + count + prev;
+                    count = 0;
+                    prev = ch;
+                }
+                count++;
+            }
+            else
+            {
+                ans.append(count + "" + prev);
+                //ans = ans + count + prev; 
+                count = 1;
+                prev = ch;
+            }
+            i++;
+        }
+
+        if(prev == word.charAt(n-1))
+        {
+            ans.append(count + "" + prev);
+        }
+        else
+        {
+            ans.append("1"+word.charAt(n-1));
+        }
+        //return ans;
+        return ans.toString();
     }
 }

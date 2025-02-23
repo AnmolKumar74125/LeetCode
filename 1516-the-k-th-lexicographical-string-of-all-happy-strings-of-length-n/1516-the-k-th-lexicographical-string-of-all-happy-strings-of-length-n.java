993bd3e78844d213871db85d768da38b267cf3a0
ANMOL KUMAR <54698801+AnmolKumar74125@users.noreply.github.com>
2025-02-24 01:42:34 +0530

    Time: 272 ms (5.06%), Space: 46 MB (5.34%) - LeetHub

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -0,0 +1,83 @@
+class Solution {
+    public String getHappyString(int n, int k) {
+        char arr[] = {'a', 'b', 'c'};
+        String s = "a";
+        List<String> temp = new ArrayList<String>();
+        for(int i = 1; i < n; i++)
+        {
+            if(s.charAt(i-1) == 'a')
+            {
+                s += "b";
+            }
+            else
+            {
+                s += "a";
+            }
+        }
+        generate(s, s.length()-1, temp, n, k);
+        System.out.println(temp);
+        return (k <= temp.size())?temp.get(k-1): "";
+    }
+
+    public void generate(String s, int ind, List<String> temp, int n, int k)
+    {
+        if( k < 0 || ind < 0)
+        {
+            return;
+        }
+        char ch = s.charAt(ind);
+        if(ch == 'c')
+        {
+            if(ind == 0)
+            {
+                System.out.println(s+".   "+k);
+                if(isHappy(s))
+                {
+                    temp.add(s);
+                }
+            }
+            generate(s, ind-1, temp, n, k);
+        }
+        else
+        {
+            System.out.println(s+".   "+k);
+            if(isHappy(s))
+            {
+                temp.add(s);
+                k = k - 1;
+            }
+            ch = (char)(ch + 1);
+            s = s.substring(0, ind) + ch;
+            System.out.println("s = "+s);
+            for(int i = s.length(); i < n; i++)
+            {
+                if(s.charAt(i-1) == 'a')
+                {
+                    s += "b";
+                }
+                else
+                {
+                    s += "a";
+                }
+            }
+            ind = s.length()-1;
+           // if(isHappy(s))
+            {
+                generate(s, ind, temp, n, k);    
+            }
+           // generate(s, ind-1, temp, n, k-1);
+        }
+    }
+
+    public boolean isHappy(String s)
+    {
+        for(int i = 1; i < s.length(); i++)
+        {
+            if(s.charAt(i-1) == s.charAt(i))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}

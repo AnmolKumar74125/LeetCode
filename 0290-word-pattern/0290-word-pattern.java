696be61381edb4b1a687c85756f768c57321e1e3
ANMOL KUMAR <54698801+AnmolKumar74125@users.noreply.github.com>
2025-03-11 00:21:27 +0530

    Time: 2 ms (12.31%), Space: 41.5 MB (48.24%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        
+        Map<Character, String> hm = new HashMap<Character, String>();
+        Map<String, Character> hm2 = new HashMap<String, Character>();
+        s = s+" ";
+        int i = 0, k = 0, n1 = pattern.length(), n2 = s.length();
+        while(k < n2 && i < n1)
+        {
+            char ch = pattern.charAt(i);
+            int x = s.indexOf(' ',k);
+            String w = s.substring(k,x);
+            k = x + 1;
+            i++;
+            if(hm.containsKey(ch) || hm2.containsKey(w))
+            {
+                if(!w.equals(hm.getOrDefault(ch,"")) || ch != hm2.getOrDefault(w,' '))
+                {
+                    return false;
+                }
+                continue;
+            }
+            hm.put(ch, w);
+            hm2.put(w, ch);
+        }
+        System.out.println(hm);
+        System.out.println(hm2);
+        if(k < n2 || i < n1)
+        {
+            return false;
+        }
+        return true;
+    }
+}

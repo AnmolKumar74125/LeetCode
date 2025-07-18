cda3f40e70fbe8a763175894d12fa3fffcb03a34
ANMOL KUMAR <54698801+AnmolKumar74125@users.noreply.github.com>
2025-07-18 20:39:16 +0530

    Time: 0 ms (100%), Space: 28.4 MB (36.32%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -3,29 +3,19 @@ public:
     int minimumOperations(vector<int>& nums) {
 
         map<int,int> hm;
-        int ans = 0, last = -1;
+        int last = 0;
         for(int i = 0; i < nums.size(); i++)
         {
-            //print_map(hm);
-            cout<<endl;
             if(hm.count(nums[i]) > 0)
             {
                 if(hm[nums[i]] >= last)
                 {
                     int x = (hm[nums[i]] / 3) + 1;
                     last = x * 3;
-                    ans = x;
                 }
             }
             hm[nums[i]] = i;
         }
-        return ans;
-    }
-    void print_map(map<int,int>& hm)
-    {
-        for(auto i : hm)
-        {
-            cout << i.first <<" : "<<i.second<<endl;
-        }
+        return last / 3;
     }
 };

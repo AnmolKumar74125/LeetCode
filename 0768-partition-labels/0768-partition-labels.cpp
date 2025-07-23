949c168bc733346d7ae7da103a19f811c9cebabd
ANMOL KUMAR <54698801+AnmolKumar74125@users.noreply.github.com>
2025-07-24 02:59:55 +0530

    Time: 11 ms (5.16%), Space: 19.9 MB (5.57%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.cpp
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -2,56 +2,33 @@ class Solution {
 public:
     vector<int> partitionLabels(string s) {
         
-        vector<vector<int>> front;
         vector<vector<int>> back;
         vector<int> max_arr(26,0);
         vector<int> ans;
         for(int i = 0; i < s.length(); i++)
         {
-            front.push_back(vector(26,0));
             back.push_back(vector(26,0));
         }
-        front[0][s[0]-97] = 1;
         back[s.length() - 1][s[s.length() - 1] - 97] = 1;
         for(int i = 1; i < s.length(); i++)
         {
-            front[i] = front[i-1];
-            //front[i][s[i]-97] = front[i - 1][s[i] - 97] + 1;
-            front[i][s[i]-97] = front[i][s[i] - 97] + 1;
             back[s.length() - 1 - i] = back[s.length() - i];
-            //back[s.length() - 1 - i][s[s.length() - 1] - 97] = back[s.length() - i][s[s.length() - 1] - 97] + 1;
             back[s.length() - 1 - i][s[s.length() - i - 1] - 97]++;
         }
-
-        #if 0
-        for(int i = 0; i < s.length(); i++)
-        {
-            cout<<s[i]<<" == Front = ";
-            for(int j = 0; j < 26; j++)
-            {
-                cout << front[i][j] <<" ";
-            }
-            cout<<endl <<s[i]<< " == Back = ";
-            for(int j = 0; j < 26; j++)
-            {
-                cout << back[i][j] <<" ";
-            }
-            cout<<endl;
-        }
-        #endif
-        max_arr = front[s.length() - 1];
+        
+        max_arr = back[0];
         int last = -1;
+        vector<int> curr(26,0);
         for(int i = 0; i < s.length(); i++)
         {
             bool f = true;
-            //cout<<"max_arr[ "<<(s[i] - 97) <<"] = " <<max_arr[s[i]-97]<<endl; 
-            if(max_arr[s[i]-97] == front[i][s[i]-97])
+            curr[s[i]-97]++;
+            if(max_arr[s[i]-97] == curr[s[i]-97])
             {
                 for(int j = 0; j < 26; j++)
                 {
-                    if((front[i][j] > 0 && back[i][j] != 0) &&  j != s[i]- 97)
+                    if((curr[j] > 0 && back[i][j] != 0) &&  j != s[i]- 97)
                     {
-                        //cout<< " case false for i = "<<i<<"  j = "<< j<<endl;
                         f = false;
                     }
                 }

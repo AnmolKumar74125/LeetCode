aa0a8f6151adf7dcf7b4e6945e5b408c47637952
ANMOL KUMAR <54698801+AnmolKumar74125@users.noreply.github.com>
2025-07-24 20:21:04 +0530

    Time: 7 ms (56.7%), Space: 29.4 MB (26.82%) - LeetHub

--- 2716-prime-subtraction-operation/2716-prime-subtraction-operation.cpp
+++ 2716-prime-subtraction-operation/2716-prime-subtraction-operation.cpp
@@ -1,79 +1,55 @@
 class Solution {
 public:
     bool primeSubOperation(vector<int>& nums) {
-        
+
         vector<int> primes;
-        for(int i = 2;i <= 1000; i++)
-        {
-            if(prime(i))
-            {
+        for (int i = 2; i <= 1000; i++) {
+            if (prime(i)) {
                 primes.push_back(i);
             }
         }
-        for(int i = nums.size() - 2; i >= 0; i--)
-        {
-            if(nums[i] < nums[i+1])
-            {
+        for (int i = nums.size() - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
                 continue;
             }
-            int diff = nums[i] - nums[i+1];
-         //   for(int j = 0; j < primes.size(); j++)
-            {
-                int x = findval(primes, diff);
-                cout<<" x = "<<x <<endl;
-                if(x > diff && x < nums[i])
-                {
-                    nums[i] = nums[i] - x;
-                    //break;
-                }
+            int diff = nums[i] - nums[i + 1];
+            int x = findval(primes, diff);
+
+            if (x > diff && x < nums[i]) {
+                nums[i] = nums[i] - x;
             }
-            if(nums[i] >= nums[i+1])
-            {
+            if (nums[i] >= nums[i + 1]) {
                 return false;
             }
         }
         return true;
     }
-    int findval(vector<int>& primes, int val)
-    {
-        int l = 0, h = primes.size()-2;
-        while(l <= h)
-        {
+    int findval(vector<int>& primes, int val) {
+        int l = 0, h = primes.size() - 2;
+        while (l <= h) {
             int mid = (l + h) / 2;
-            //cout<< "mid = " << mid <<" l = "<<l<<" h = "<<h<<" primes[mid] = " << primes[mid]<<"val = " << val <<endl;
-            if(primes[mid] == val || (primes[mid] < val && primes[mid+1] > val))
-            {
+            if (primes[mid] == val ||
+                (primes[mid] < val && primes[mid + 1] > val)) {
                 return primes[mid + 1];
-            }
-            else if(mid == 0 && primes[mid] > val)
-            {
-              return primes[mid];
-            }
-            else if(primes[mid] > val)
-            {
-                h = mid-1;
-            }
-            else
-            {
+            } else if (mid == 0 && primes[mid] > val) {
+                return primes[mid];
+            } else if (primes[mid] > val) {
+                h = mid - 1;
+            } else {
                 l = mid + 1;
             }
         }
-        return primes[primes.size()-1];
+        return primes[primes.size() - 1];
     }
-    bool prime(int num)
-    {
-        if(num == 2 || num == 3)
-        {
+    bool prime(int num) {
+        if (num == 2 || num == 3) {
             return true;
         }
-        if(num %2 == 0 || num % 3 == 0)
-        {
+        if (num % 2 == 0 || num % 3 == 0) {
             return false;
         }
-        for(int i = 5; i <= (int)sqrt(num); i+= 6)
-        {
-            if(num % i == 0 || num % (i+2) == 0)
-            {
+        for (int i = 5; i <= (int)sqrt(num); i += 6) {
+            if (num % i == 0 || num % (i + 2) == 0) {
                 return false;
             }
         }

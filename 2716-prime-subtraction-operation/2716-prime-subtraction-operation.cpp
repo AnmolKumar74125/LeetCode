b20f65a7c464bd6021674d974aad8d06fbc1e1e9
ANMOL KUMAR <54698801+AnmolKumar74125@users.noreply.github.com>
2025-07-24 20:20:17 +0530

    Time: 12 ms (42.91%), Space: 29.2 MB (36.78%) - LeetHub

--- 2716-prime-subtraction-operation/2716-prime-subtraction-operation.cpp
+++ 2716-prime-subtraction-operation/2716-prime-subtraction-operation.cpp
@@ -17,12 +17,14 @@ public:
                 continue;
             }
             int diff = nums[i] - nums[i+1];
-            for(int j = 0; j < primes.size(); j++)
+         //   for(int j = 0; j < primes.size(); j++)
             {
-                if(primes[j] > diff && primes[j] < nums[i])
+                int x = findval(primes, diff);
+                cout<<" x = "<<x <<endl;
+                if(x > diff && x < nums[i])
                 {
-                    nums[i] = nums[i] - primes[j];
-                    break;
+                    nums[i] = nums[i] - x;
+                    //break;
                 }
             }
             if(nums[i] >= nums[i+1])
@@ -31,6 +33,32 @@ public:
             }
         }
         return true;
+    }
+    int findval(vector<int>& primes, int val)
+    {
+        int l = 0, h = primes.size()-2;
+        while(l <= h)
+        {
+            int mid = (l + h) / 2;
+            //cout<< "mid = " << mid <<" l = "<<l<<" h = "<<h<<" primes[mid] = " << primes[mid]<<"val = " << val <<endl;
+            if(primes[mid] == val || (primes[mid] < val && primes[mid+1] > val))
+            {
+                return primes[mid + 1];
+            }
+            else if(mid == 0 && primes[mid] > val)
+            {
+              return primes[mid];
+            }
+            else if(primes[mid] > val)
+            {
+                h = mid-1;
+            }
+            else
+            {
+                l = mid + 1;
+            }
+        }
+        return primes[primes.size()-1];
     }
     bool prime(int num)
     {

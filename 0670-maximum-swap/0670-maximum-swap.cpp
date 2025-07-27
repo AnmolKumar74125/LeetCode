fac3da2f4e797cebc43adcb501f4db811b57160a
ANMOL KUMAR <54698801+AnmolKumar74125@users.noreply.github.com>
2025-07-27 16:01:12 +0530

    Time: 3 ms (1.28%), Space: 8.2 MB (12.52%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+        
+        int p = num;
+        vector<int> arr;
+        arr.push_back(0);
+        string s = "";
+        while(p != 0)
+        {
+            int r = p%10;
+            s.push_back((char)(r+48));
+            arr.push_back(max(r,arr[arr.size()-1]));
+            p = p/10;
+        }
+        reverse(arr.begin(),arr.end());
+        reverse(s.begin(),s.end());
+        cout<<"s = "<<s<<endl;
+        int pos1 = -1,v1 = -1, x = -1;
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(((int)(s[i]) - 48) < arr[i+1])
+            {
+                pos1 = i;
+                v1 = arr[i+1];
+                x = ((int)(s[i])-48);
+                break;
+            }
+        }
+        for(int i = 0; i < arr.size(); i++)
+        {
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+        string w = "";
+        bool f = false;
+        int ans = 0;
+        cout<<"pos1 = "<<pos1 << " v1 = "<<v1<< " x = "<<x<<endl;
+        for(int i = s.length()-1; i >= 0; i--)
+        //for(int i = 0; i < s.length(); i++)
+        {
+            if(((int)(s[i])-48) == v1 && f==false)
+            {
+                w = (char)(x + 48) + w;
+                f = true;
+                continue;
+            }
+            if(i == pos1)
+            {
+                w = (char)(v1 + 48) + w;
+                continue;
+            }
+            w = s[i] + w;
+        }
+        for(int i = 0; i < w.length(); i++)
+        {
+            ans =ans * 10 + (int)(w[i] - 48);   
+        }
+        cout<<"w = "<<w<<endl;
+        return ans;
+    }
+};

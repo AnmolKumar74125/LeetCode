898d981ba883a190784388e49c9260bee8c430de
ANMOL KUMAR <54698801+AnmolKumar74125@users.noreply.github.com>
2026-01-11 00:03:10 +0530

    Time: 2 ms (89.25%), Space: 48 MB (9.44%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int k=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==0)
+            {
+                continue;
+            }
+            nums[k++]=nums[i];
+        }
+        while(k<nums.length)
+        {
+            nums[k++]=0;
+        }
+    }
+}

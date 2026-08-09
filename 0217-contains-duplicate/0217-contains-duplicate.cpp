fa454cf77886c50bdd6098b403ac568176a0d84e
ANMOL KUMAR <54698801+AnmolKumar74125@users.noreply.github.com>
2026-08-09 19:01:20 +0530

    Time: 72 ms (41.58%), Space: 111.4 MB (20.12%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        unordered_set<int> hs;
+        for(int i = 0; i< nums.size(); i++)
+        {
+            if(hs.contains(nums[i]))
+            {
+                return true;
+            }
+            hs.insert(nums[i]);
+        }
+        return false;
+    }
+};
